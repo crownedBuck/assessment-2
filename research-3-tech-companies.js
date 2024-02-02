e2f36ada206e9cf85f2baea6e98f84673de16850
Laura Hart <157418980+crownedBuck@users.noreply.github.com>
2024-02-02 14:17:03 -0600

    "trying it manually"

---
+++ research-3-tech-companies.js
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////
+///////////////////RESEARCH-3-TECH-CPMPANIES.JS/////////////////////
+////////////////////////////////////////////////////
+
+/*
+Apple - 
+    - Swift,Objective-C, Cocoa Touch (iOS), Cocoa (OS X), WatchOS, TvOS, Framer, OpenStreetMap, Xcode, Pingdom, TeamCity, XTML, PostgreSQL
+
+Google
+    - Java, C, C++, .NET, Go, Python, shell, Perl, Javascript, SQL
+    // Side note - Dear god, they want at least a master's degree for a Data Scientest with 8 years of experience OR a Data Scientest with a PHD with 6 years of experience??? WTF https://www.google.com/about/careers/applications/jobs/results/76891892924457670-data-scientist-engineering-and-operations-portuguese-english
+
+Twitter // I REFUSE to call it X. 
+    - Scala/Java, Python, Rust, MySQL, Node.js, Swift, Objective-C, Kotlin, RESTful API, GraphQL, 
+    // Side note - I would not work there while Elon is there. It seems to be a pretty hostile work enviornment
+
+*/
+
+// Why isn't this showing up?-tech
+
+let frog = 0
+
+console.log(frog) // doing this to see if this will push it to github
